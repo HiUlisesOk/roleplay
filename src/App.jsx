58dd84b4d8b29a21cb631c85fefbf28f92a61b74
FacundoEsquivel97 <esquivelfacundo.97@gmail.com>
2023-06-18 20:58:41 -0300

    Navbar reestructurada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Landing from "./Pages/Landing.jsx"
 import Home from "./Pages/Home.jsx"
 import Register from "./Pages/Register.jsx"
-
+import Playground from "./Pages/Playground.jsx"
 import { Routes, Route}  from "react-router-dom"
 
 export default function App() {
@@ -12,6 +12,7 @@ export default function App() {
     <Route path="/" element={<Landing />} />
     <Route path="/home" element={<Home />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/playground" element={<Playground />} />
   </Routes>
   </>
   )
---
+++ src/Pages/Playground.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar.jsx"
+
+export default function Playground() {
+    return(
+        <>
+        <Navbar />
+        </>
+    )
+}
