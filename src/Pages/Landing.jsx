1df4ee9a54b7db607f4e93864ff4c09c280f2b0c
Ulises Esquivel <112531555+HiUlisesOk@users.noreply.github.com>
2023-06-18 13:38:43 -0300

    :recycle: IGM BANNER REGISTER

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,56 +1,59 @@
 import { Box, Typography, TextField } from '@mui/material'
 import Button from '@mui/material/Button'
 import { Link } from "react-router-dom";
+import BG from '../img/bg.jpg'
+
 
 export default function Landing() {
-  return(
+  return (
     <>
-    <Box sx={{
-      display: 'flex',
-      justifyContent : 'center',
-      height: '100vh',
-      backgroundColor: '#1CB251',
-    }}>
-      <Box
-      component={'img'}
-      src='https://i.imgur.com/sW11viE.jpg'
-      alt="background-image"
-        sx={{
-          height: '100vh',
-          width: '100vw',
-          objectFit: 'cover',  
-          opacity: 0.8,
-        }}
-      />
-    </Box>
-    <Box sx={{
-      position: 'absolute',
-      top: 0,
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-    }}>
-      <Box>
-      <Typography variant="h3" align='center' sx={{color: '#f5f5f5'}}>ROLEPLAY</Typography>
-      <Typography variant="subtitle2" align='center' sx={{color: '#f5f5f5'}}>Lorem ipsum dolor sit amet consectetur.</Typography>
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#1CB251',
+      }}>
+        <Box
+          component={'img'}
+          src={BG}
+          alt="background-image"
+          sx={{
+            height: '100vh',
+            width: '100vw',
+            objectFit: 'cover',
+            opacity: 0.8,
+          }}
+        />
       </Box>
-      <Box component="form" sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 1}}>
-      <TextField id="username" label="Nombre de usario" variant="filled" />
-      <TextField id="password" label="Contraseña" variant="filled" />
-      <Button component={Link} to="/home" variant="contained" color="primary">
-        ENTRAR
-      </Button>
-      <Button component={Link} to="/register" size="small" variant="text" color="primary">
-        REGISTRARSE
-      </Button>
+      <Box sx={{
+        position: 'absolute',
+        top: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+        <Box>
+          <Typography variant="h3" align='center' sx={{ color: '#f5f5f5' }}>ROLEPLAY</Typography>
+          <Typography variant="subtitle2" align='center' sx={{ color: '#f5f5f5' }}>Lorem ipsum dolor sit amet consectetur.</Typography>
+        </Box>
+        <Box component="form" sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 1
+        }}>
+          <TextField id="username" label="Nombre de usario" variant="filled" />
+          <TextField id="password" label="Contraseña" variant="filled" />
+          <Button component={Link} to="/home" variant="contained" color="primary">
+            ENTRAR
+          </Button>
+          <Button component={Link} to="/register" size="small" variant="text" color="primary">
+            REGISTRARSE
+          </Button>
+        </Box>
       </Box>
-    </Box>
     </>
   )
 }
